5cde3a1d193d5a5f5f077c38c109901fa5224584
Stars Tracker <techixspotify@gmail.com>
2021-09-24 10:38:53 +0530

    feat : Add Owner action

---
+++ src/actions/general/Owner.ts
@@ -0,0 +1,18 @@
+import Actions from "../../structures/Actions";
+import Darwin from "./../../Darwin";
+import Message from "./../../structures/Message";
+
+export default class Greetings extends Actions {
+  public constructor(Darwin: Darwin) {
+    super(Darwin, {
+      name: "Greetings",
+      match: new RegExp("^who (made|created) you", "i"),
+    });
+  }
+  public async exec(deviceID: number, message: string): Promise<void> {
+    return new Message(this.Darwin, deviceID)
+      .say("I was created by Stars Tracker!")
+      .setContent("I was created by Stars Tracker")
+      .dispatch();
+  }
+}
